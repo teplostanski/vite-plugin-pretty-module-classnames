4349393f26e31a94706f6f120e7fab4a9c9172e8
teplostanski <teplostanski@yandex.ru>
2024-08-11 17:54:19 +0300

    feat: Add configurable hash length
    Added a check using Math.max to ensure a minimum hash length of 5 characters.
    The variable finalHashLength now always holds a value of at least 5, even if hashLength is set to a lower value.
    This prevents the creation of overly short hashes, ensuring uniqueness and security.

---
+++ src/constats.ts
@@ -0,0 +1,5 @@
+export const ERROR_FILENAME_UNDEFINED =
+  "The filename must be a string and cannot be undefined.";
+export const ERROR_FILENAME_INVALID = "Filename must include a valid file name.";
+export const ERROR_GENERATE_SCOPED_NAME_CONFLICT = `Custom settings for generateScopedName are already set.
+The vite-plugin-pretty-module-classnames plugin cannot be used with other generateScopedName settings.`;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
 import type { Plugin, UserConfig } from "vite";
 import { createHash } from "crypto";
+import { ERROR_FILENAME_UNDEFINED, ERROR_FILENAME_INVALID, ERROR_GENERATE_SCOPED_NAME_CONFLICT } from "./constats.js";
 
-function getHash(input: string): string {
-  return createHash("sha256").update(input).digest("hex").slice(0, 5);
+interface PrettyModuleClassnamesOptions {
+  lineNumber?: boolean;
+  hashLength?: number;
+}
+
+function getHash(input: string, length: number): string {
+  return createHash("sha256").update(input).digest("hex").slice(0, length);
+}
+
+/**
+ * Computes the line number where the CSS class is defined.
+ *
+ * @param name - The name of the CSS class.
+ * @param css - The CSS code as a string.
+ * @returns The line number where the CSS class is found, or undefined if not found.
+ */
+function computeLineNumber(name: string, css: string): number | undefined {
+  const lines = css.split("\n");
+  const match = new RegExp(`\\.${name}\\b`);
+  const lineNumber = lines.findIndex((line) => match.test(line));
+  return lineNumber !== -1 ? lineNumber + 1 : undefined;
 }
 
 /**
@@ -11,31 +31,33 @@ function getHash(input: string): string {
  * If `lineNumber` is provided, it's added to the generated class name.
  *
  * @param name - The name of the CSS class that will be part of the generated unique name.
- * @param filename is the full path to the style file, which can be undefined. If the parameter is undefined, the function throws an exception.
- * @param lineNumber is the line number where the CSS class is defined, optional.
+ * @param filename - The full path to the style file, which can be undefined. If the parameter is undefined, the function throws an exception.
+ * @param lineNumber - The line number where the CSS class is defined, optional.
+ * @param hashLength - The length of the hash to be generated, default is 5.
  * @returns A string with a unique class name, including a sanitized file name, the original class name, a randomly generated hash, and optionally the line number.
  * @throws Error if `filename` is not provided or is not a string.
  */
 function sanitizeModuleClassname(
   name: string,
   filename: string | undefined,
-  lineNumber?: number
+  lineNumber?: number,
+  hashLength: number = 5
 ): string {
   if (typeof filename !== "string") {
-    throw new Error("The filename must be string and cannot be undefined.");
+    throw new Error(ERROR_FILENAME_UNDEFINED);
   }
 
-  const parts = filename.split("?")[0].split("/");
-  const lastSegment = parts.pop();
-
-  if (!lastSegment) {
-    throw new Error("Filename must include a valid file name.");
+  const sanitizedFilename = filename.split("?")[0].split("/").pop();
+  if (!sanitizedFilename) {
+    throw new Error(ERROR_FILENAME_INVALID);
   }
 
-  const baseFilename = lastSegment.replace(/(\.vue|\.module)?(\.\w+)$/, "");
-
+  const baseFilename = sanitizedFilename.replace(
+    /(\.vue|\.module)?(\.\w+)$/,
+    ""
+  );
   const classname = `${baseFilename}__${name}`;
-  const hash = getHash(`${classname}`);
+  const hash = getHash(classname, hashLength);
   const lineInfo = lineNumber !== undefined ? `-${lineNumber}` : "";
 
   return `${classname}_${hash}${lineInfo}`;
@@ -44,13 +66,19 @@ function sanitizeModuleClassname(
 /**
  * Adds the filename without the `-module` suffix to the class names of CSS modules.
  * It customizes the generateScopedName function to use a sanitized version of the filename, class name, and a hash.
- * If the `lineNumber` option is set to true, the line number is added to the generated class name.
  *
  * @prop {Object} `options` - Plugin options.
  * @prop {boolean} `options.lineNumber` - Whether to include the line number in the generated class name. @default false
+ * @prop {number} `options.hashLength` - Length of the hash to be included in the generated class name. The minimum value is 5. @default 5
  * @returns {Plugin} A Vite plugin object with a custom configuration for CSS modules.
  */
-export default function PrettyModuleClassnames(options: { lineNumber?: boolean } = {}): Plugin {
+
+export default function PrettyModuleClassnames(
+  options: PrettyModuleClassnamesOptions = {}
+): Plugin {
+  const { lineNumber = false, hashLength = 5 } = options;
+  const finalHashLength = Math.max(hashLength, 5);
+
   return {
     name: "vite-plugin-pretty-module-classnames",
     /**
@@ -59,32 +87,34 @@ export default function PrettyModuleClassnames(options: { lineNumber?: boolean }
      * If not set, it applies a custom generateScopedName function.
      *
      * @param {UserConfig} config - The original Vite configuration provided by the user.
-     * @throws {Error} Throws an error if generateScopedName is already defined in the user's configuration.
      * @returns {UserConfig} A modified Vite configuration object with custom settings for CSS module class name generation.
+     * @throws {Error} Throws an error if generateScopedName is already defined in the user's configuration.
      */
     config(config: UserConfig): UserConfig {
+      const cssModulesOptions = config.css?.modules;
+
       if (
-        typeof config.css?.modules === "object" &&
-        config.css.modules.generateScopedName
+        typeof cssModulesOptions === "object" &&
+        cssModulesOptions.generateScopedName
       ) {
-        throw new Error(
-          "Custom settings for generateScopedName are already set. The vite-plugin-pretty-module-classnames plugin cannot be used with other generateScopedName settings."
-        );
+        throw new Error(ERROR_GENERATE_SCOPED_NAME_CONFLICT);
       }
 
       // If generateScopedName is not set, apply the plugin's configuration
       const newCssConfig = {
         ...config.css,
         modules: {
-          ...config.css?.modules,
+          ...cssModulesOptions,
           generateScopedName: (name: string, filename: string, css: string) => {
-            let lineNumber: number | undefined;
-            if (options.lineNumber) {
-              const lines = css.split('\n');
-              const match = new RegExp(`\\.${name}\\b`);
-              lineNumber = lines.findIndex(line => match.test(line)) + 1;
-            }
-            return sanitizeModuleClassname(name, filename, lineNumber);
+            const computedLineNumber = lineNumber
+              ? computeLineNumber(name, css)
+              : undefined;
+            return sanitizeModuleClassname(
+              name,
+              filename,
+              computedLineNumber,
+              finalHashLength
+            );
           },
         },
       };
